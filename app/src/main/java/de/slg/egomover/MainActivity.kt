b7e98cdb0866d9582ff04c3470f46eb9eab7fc47
lca311 <spitzer-webdesign@outlook.de>
2018-04-13 17:15:16 +0200

    Added MainActivity

--- app/src/main/java/de/slg/egomover/MainActivity.kt
+++ app/src/main/java/de/slg/egomover/MainActivity.kt
@@ -1,3 +1,15 @@
 package de.slg.egomover
 
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(b : Bundle?) {
+
+        super.onCreate(b)
+        //TODO
+
+    }
+
+}
