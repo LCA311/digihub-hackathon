19f570634647dbb755be0e5f797944aeb00044a4
Sir_M <mirko.castor@gmail.com>
2018-04-14 00:12:19 +0200

    Commit #1

--- app/src/main/java/de/slg/egomover/MainActivity.kt
+++ app/src/main/java/de/slg/egomover/MainActivity.kt
@@ -1,15 +1,33 @@
 package de.slg.egomover
 
+import android.app.Activity
 import android.os.Bundle
+import android.support.v4.app.FragmentActivity
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.android.gms.maps.MapView
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(b : Bundle?) {
+
+
+    override fun onCreate(b: Bundle?) {
 
         super.onCreate(b)
-        //TODO
+        setContentView(R.layout.activity_main)
+
+      //  supportFragmentManager.inTransaction {
+      //      add(R.id.mapView, )
+      //  }
 
     }
+    inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> Unit) {
+        val fragmentTransaction = beginTransaction()
+        fragmentTransaction.func()
+        fragmentTransaction.commit()
+    }
+
 
 }
